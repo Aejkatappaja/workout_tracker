fb64620a3beeeac9c12ad2825e62e28713ab69bb
Aejkatappaja <joverjlj@gmail.com>
2026-07-06 16:53:13 +0200

    feat: graceful shutdown, PORT env, and DB-ping health check

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 	"time"
 
 	"github.com/Aejkatappaja/go-gym/internal/app"
@@ -11,30 +16,44 @@ import (
 )
 
 func main() {
+	defaultPort := 8080
+	if p, err := strconv.Atoi(os.Getenv("PORT")); err == nil && p > 0 {
+		defaultPort = p
+	}
 	var port int
-	flag.IntVar(&port, "port", 8080, "go back server port")
+	flag.IntVar(&port, "port", defaultPort, "HTTP server port")
 	flag.Parse()
 
-	app, err := app.NewApplication()
+	application, err := app.NewApplication()
 	if err != nil {
 		panic(err)
 	}
+	defer func() { _ = application.DB.Close() }()
 
-	defer func() { _ = app.DB.Close() }()
-
-	r := routes.SetupRoutes(app)
 	server := &http.Server{
 		Addr:         fmt.Sprintf(":%d", port),
-		Handler:      r,
+		Handler:      routes.SetupRoutes(application),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
-	app.Logger.Printf("we are running on port %d\n", port)
+	go func() {
+		application.Logger.Printf("listening on port %d", port)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			application.Logger.Fatalf("server error: %v", err)
+		}
+	}()
 
-	err = server.ListenAndServe()
-	if err != nil {
-		app.Logger.Fatal(err)
+	// graceful shutdown on SIGINT / SIGTERM (container stop)
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+
+	application.Logger.Println("shutting down")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		application.Logger.Printf("graceful shutdown failed: %v", err)
 	}
 }
